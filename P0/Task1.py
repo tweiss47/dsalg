36f94e2f677737b73dbbd1c93fc239bc46f34252
Taylor Weiss <taylor.weiss@comcast.net>
2019-04-29 15:58:42 -0700

    Second task.

--- P0/Task1.py
+++ P0/Task1.py
@@ -14,7 +14,18 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+numbers = set()
+for source, dest, time in texts:
+    numbers.add(source)
+    numbers.add(dest)
+
+for source, dest, time, duration in calls:
+    numbers.add(source)
+    numbers.add(dest)
+
+print("There are", len(numbers), "different telephone numbers in the records.")
+

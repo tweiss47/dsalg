3500762735f0b680188cd616ccdce22678cb6ee7
Taylor Weiss <taylor.weiss@comcast.net>
2019-05-26 09:09:29 -0700

    Final refactoring of Huffman coding

--- ds-project/huffman/huffman.py
+++ ds-project/huffman/huffman.py
@@ -2,6 +2,40 @@ import sys
 import heapq
 
 
+class HuffNode:
+    '''
+    Node class for building a Huffman encoding tree.
+    '''
+    def __init__(self, count, char=None):
+        self.count = count
+        self.char = char
+        self.left = None
+        self.right = None
+
+    # override the rich comparison methods so we can sort our nodes during tree
+    # building
+    def __eq__(self, other):
+        return self.count.__eq__(other.count)
+
+    def __ne__(self, other):
+        return self.count.__ne__(other.count)
+
+    def __lt__(self, other):
+        return self.count.__lt__(other.count)
+
+    def __le__(self, other):
+        return self.count.__le__(other.count)
+
+    def __gt__(self, other):
+        return self.count.__gt__(other.count)
+
+    def __ge__(self, other):
+        return self.count.__ge__(other.count)
+
+    def __repr__(self):
+        return f'Node({self.count},{self.char})'
+
+
 def huffman_encoding(data):
     '''
     Encode the data into a string of 0/1 characters, suitable for compaction
@@ -46,77 +80,18 @@ def build_huffman_tree(data):
 
     Returns: the HuffNode object at the root of the tree
     '''
-    frequency_map = get_character_frequency(data)
-    return build_tree(frequency_map)
+    # Build of dictionary of character frequencies
+    frequency_map = dict()
+    for char in data:
+        count = frequency_map.get(char, 0)
+        frequency_map[char] = count + 1
 
-
-def encode_with_tree(data, root):
-    '''
-    Encode the input data with the Huffman tree (root)
-
-    Returns: the data encoded as a string of 0/1 characters
-    '''
-    code_map = tree_to_code_map(root)
-    return encode_string(a_great_sentence, code_map)
-
-
-def get_character_frequency(s):
-    '''
-    Take a string (s) and determine the relevant frequencies of the
-    characters
-
-    Returns:
-        map of character frequencies {'a': 1, 'h': 1}
-    '''
-    result = dict()
-    for c in s:
-        count = result.get(c, 0)
-        result[c] = count + 1
-    return result
-
-
-class HuffNode:
-    def __init__(self, count, char=None):
-        self.count = count
-        self.char = char
-        self.left = None
-        self.right = None
-
-    # override the rich comparison methods so we can sort our nodes during tree
-    # building
-    def __eq__(self, other):
-        return self.count.__eq__(other.count)
-
-    def __ne__(self, other):
-        return self.count.__ne__(other.count)
-
-    def __lt__(self, other):
-        return self.count.__lt__(other.count)
-
-    def __le__(self, other):
-        return self.count.__le__(other.count)
-
-    def __gt__(self, other):
-        return self.count.__gt__(other.count)
-
-    def __ge__(self, other):
-        return self.count.__ge__(other.count)
-
-    def __repr__(self):
-        return f'Node({self.count},{self.char})'
-
-
-def build_tree(frequency_map):
-    '''
-    Build the Huffman Tree by assigning a binary code to each letter, using
-    codes for the more frequent letters. (This is the heart of the
-    algorithm.)
-    '''
     # Use character frequencies to build a heap of HuffNodes
     nodes = []
     for key, value in frequency_map.items():
         heapq.heappush(nodes, HuffNode(value, key))
 
+    # Build the tree by combining nodes with the smallest count values
     while (len(nodes) > 1):
         # pull two nodes off the front of the heap
         left = heapq.heappop(nodes)
@@ -129,16 +104,23 @@ def build_tree(frequency_map):
 
         # insert the node back into the heap
         heapq.heappush(nodes, node)
+
     return nodes[0]
 
 
-def tree_to_code_map(root):
+def encode_with_tree(data, root):
     '''
-    Build a map of the character to code strings
+    Encode the input data with the Huffman tree (root)
+
+    Returns: the data encoded as a string of 0/1 characters
     '''
-    # traverse the tree when you hit a leaf insert the char and code into a map
+    # Build a map of characters to codes from the tree rather than traversing
+    # for each character
     code_map = dict()
+
+    # recursive helper to traverse the tree
     def traverse(node, code):
+        # when you hit a leaf insert the char and code into code_map
         if node.char:
             code_map[node.char] = code
             return
@@ -147,14 +129,11 @@ def tree_to_code_map(root):
         if node.right:
             traverse(node.right, code + '1')
 
+    # invoke the traversal
     traverse(root, '')
-    return code_map
 
-
-def encode_string(s, code_map):
-    chars = []
-    for char in s:
-        chars.append(code_map[char])
+    # use the map to encode the data
+    chars = [code_map[char] for char in data]
     return ''.join(chars)
 
 
